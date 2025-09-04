f91f02d8a2a12cccb55e4112f6299b5cd44a4797
mateushmd <mateushmdiniz@gmail.com>
2025-09-04 11:03:14 -0300

    adds

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -1,5 +1,14 @@
 use super::CPU;
 
+const B   : u8 = 0;
+const C   : u8 = 1;
+const D   : u8 = 2;
+const E   : u8 = 3;
+const H   : u8 = 4;
+const L   : u8 = 5;
+const MEM : u8 = 6;
+const A   : u8 = 7;
+
 impl CPU {
     pub(super) fn mov(&mut self, inst: u8) {
         let s = inst & 0x07;
@@ -30,5 +39,24 @@ impl CPU {
         let d = (inst >> 4) & 0x03;
         let value = self.get_reg_pair(d);
         self.set_reg_pair(d, value - 1);
+    }
+
+    pub(super) fn add(&mut self, inst: u8) {
+        let s = inst & 0x07;
+        let value = self.get_reg(s);
+        let a_value = self.get_reg(A);
+        self.set_reg(A, a_value + value);
+    }
+
+    pub(super) fn adc(&mut self, inst: u8) {
+    }
+
+    pub(super) fn adi(&mut self, inst: u8) {
+    }
+
+    pub(super) fn aci(&mut self, inst: u8) {
+    }
+
+    pub(super) fn dad(&mut self, inst: u8) {
     }
 }
