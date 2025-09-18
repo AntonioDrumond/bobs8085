b89b148d3ca89a3f7e4cb1acf7ad0ae41c87a101
Antonio Drumond <adcdstom@gmail.com>
2025-09-18 16:47:47 -0300

    update shell.nix to fix graphic library

--- shell.nix
+++ shell.nix
@@ -1,14 +1,29 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-pkgs.mkShell {
+
+pkgs.mkShell rec {
   buildInputs = with pkgs; [
     rustc
     rustfmt
     cargo
-    gcc
     clippy
+    gcc
+    expat
+    fontconfig
+    freetype
+    freetype.dev
+    libGL
+    pkg-config
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+    wayland
+    libxkbcommon
   ];
+
+  LD_LIBRARY_PATH = builtins.foldl' (a: b: "${a}:${b}/lib") "${pkgs.vulkan-loader}/lib" buildInputs;
 
   shellHook = ''
     echo ""
