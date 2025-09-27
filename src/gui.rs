1cd9a052091bb2103a6355abc004e2ced305c73b
Mateus <chenri48155@gmail.com>
2025-09-27 15:45:23 -0300

    iced gap

--- src/gui.rs
+++ src/gui.rs
@@ -1,26 +1,23 @@
 #[allow(unused_imports, dead_code)]
-use bobs8085::{
-    changes::Changes,
-    Simulator,
-    assemble,
-};
+use bobs8085::{Simulator, assemble, changes::Changes};
 
-use std::{
-    fs::File,
-    io::Write,
-};
+use std::{fs::File, io::Write};
 
 #[allow(unused_imports, dead_code)]
 use iced::{
-    Element, Fill,
-    Alignment, Length,
-    Border, Color,
+    Alignment, Application, Border, Color, Element, Fill, Length, Theme, theme,
     widget::{
-        Scrollable, Row, Column,
-        row, column, text, button,
-        text_editor, scrollable, container,
+        Column, Row, Scrollable, button, center, column, container, mouse_area, opaque, row,
+        scrollable, stack, text, text_editor,
     },
 };
+
+#[derive(Debug, Clone)]
+#[allow(dead_code)]
+enum ContextModal {
+    File,
+    Edit,
+}
 
 #[derive(Debug, Clone)]
 #[allow(unused_imports, dead_code)]
@@ -29,6 +26,7 @@ enum Message {
     RunAll,
     RunStep,
     Edit(text_editor::Action),
+    ContextOpen(ContextModal),
 }
 
 #[derive(Debug)]
@@ -41,44 +39,67 @@ struct State {
 impl Default for State {
     fn default() -> Self {
         let sim = Simulator::bus_from_file("bin/fibonacci.bin");
-        State { sim, editor_content: text_editor::Content::default() }
+        State {
+            sim,
+            editor_content: text_editor::Content::default(),
+        }
     }
 }
 
-fn editor_box (state: &State) -> Column<'_, Message> {
+fn theme() -> Theme {
+    Theme::TokyoNight
+}
+
+fn editor_box(state: &State) -> Column<'_, Message> {
     column![
         text_editor(&state.editor_content)
             .on_action(Message::Edit)
             .height(Fill),
-        row![button("Assemble").on_press(Message::Assemble), button("RunAll").on_press(Message::RunAll)].spacing(10),
-    ].spacing(8)
-     .align_x(Alignment::Center)
+        row![
+            button("Assemble").on_press(Message::Assemble),
+            button("RunAll").on_press(Message::RunAll)
+        ]
+        .spacing(10),
+    ]
+    .spacing(8)
+    .align_x(Alignment::Center)
 }
 
-fn memory_box (state: &State) -> Scrollable<'_, Message> {
-
+fn memory_box(state: &State) -> Scrollable<'_, Message> {
     let header = row![
-        text("1").width(Fill), text("2").width(Fill),
-        text("3").width(Fill), text("4").width(Fill),
-        text("5").width(Fill), text("6").width(Fill),
-        text("7").width(Fill), text("8").width(Fill),
-        text("9").width(Fill), text("A").width(Fill),
-        text("B").width(Fill), text("C").width(Fill),
-        text("D").width(Fill), text("E").width(Fill),
+        text("1").width(Fill),
+        text("2").width(Fill),
+        text("3").width(Fill),
+        text("4").width(Fill),
+        text("5").width(Fill),
+        text("6").width(Fill),
+        text("7").width(Fill),
+        text("8").width(Fill),
+        text("9").width(Fill),
+        text("A").width(Fill),
+        text("B").width(Fill),
+        text("C").width(Fill),
+        text("D").width(Fill),
+        text("E").width(Fill),
         text("F").width(Fill),
-    ].spacing(13);
+    ]
+    .spacing(13);
 
     let mut mem_box = column![header];
 
     let mut i = 0xC000;
     while i < 0xCFFF {
         let mut mem_row = row![];
-        while ((i+1) % 16) != 0 {
-            mem_row = mem_row.push( text(format!("{:02X}", state.sim.mem_get8(i))).width(Fill).size(14) );
-            i+=1;
+        while ((i + 1) % 16) != 0 {
+            mem_row = mem_row.push(
+                text(format!("{:02X}", state.sim.mem_get8(i)))
+                    .width(Fill)
+                    .size(14),
+            );
+            i += 1;
         }
         mem_box = mem_box.push(mem_row.spacing(5));
-        i+=1;
+        i += 1;
     }
 
     let mem_box = scrollable(
@@ -93,31 +114,92 @@ fn memory_box (state: &State) -> Scrollable<'_, Message> {
                 background: None,
                 text_color: None,
                 shadow: Default::default(),
-            })
+            }),
     );
 
-    mem_box
-        .height(Length::Fixed(500.0))
-        .spacing(5)
+    mem_box.height(Length::Fixed(500.0)).spacing(5)
 }
 
-fn register_box (state: &State) -> Column<'_, Message> {
-
+fn register_box(state: &State) -> Column<'_, Message> {
     column![
         row![text("Register").width(Fill), text("Value").width(Fill)],
-        row![text("Accumulator").width(Fill), text(format!("{:02X}", state.sim.cpu_get_reg(7))).width(Fill)],
-        row![text("Register B").width(Fill), text(format!("{:02X}", state.sim.cpu_get_reg(0))).width(Fill)],
-        row![text("Register C").width(Fill), text(format!("{:02X}", state.sim.cpu_get_reg(1))).width(Fill)],
-        row![text("Register D").width(Fill), text(format!("{:02X}", state.sim.cpu_get_reg(2))).width(Fill)],
-        row![text("Register E").width(Fill), text(format!("{:02X}", state.sim.cpu_get_reg(3))).width(Fill)],
-        row![text("Register H").width(Fill), text(format!("{:02X}", state.sim.cpu_get_reg(4))).width(Fill)],
-        row![text("Register L").width(Fill), text(format!("{:02X}", state.sim.cpu_get_reg(5))).width(Fill)],
-        row![text("Memory").width(Fill), text(format!("{:02X}", state.sim.cpu_get_reg(6))).width(Fill)],
-    ].height(Fill)
-     .align_x(Alignment::Center)
+        row![
+            text("Accumulator").width(Fill),
+            text(format!("{:02X}", state.sim.cpu_get_reg(7))).width(Fill)
+        ],
+        row![
+            text("Register B").width(Fill),
+            text(format!("{:02X}", state.sim.cpu_get_reg(0))).width(Fill)
+        ],
+        row![
+            text("Register C").width(Fill),
+            text(format!("{:02X}", state.sim.cpu_get_reg(1))).width(Fill)
+        ],
+        row![
+            text("Register D").width(Fill),
+            text(format!("{:02X}", state.sim.cpu_get_reg(2))).width(Fill)
+        ],
+        row![
+            text("Register E").width(Fill),
+            text(format!("{:02X}", state.sim.cpu_get_reg(3))).width(Fill)
+        ],
+        row![
+            text("Register H").width(Fill),
+            text(format!("{:02X}", state.sim.cpu_get_reg(4))).width(Fill)
+        ],
+        row![
+            text("Register L").width(Fill),
+            text(format!("{:02X}", state.sim.cpu_get_reg(5))).width(Fill)
+        ],
+        row![
+            text("Memory").width(Fill),
+            text(format!("{:02X}", state.sim.cpu_get_reg(6))).width(Fill)
+        ],
+    ]
+    .height(Fill)
+    .align_x(Alignment::Center)
 }
 
-fn update (state: &mut State, message: Message) {
+fn context_menu() -> Row<'static, Message> {
+    row![
+        button("File").on_press(Message::ContextOpen(ContextModal::File)),
+        button("Edit").on_press(Message::ContextOpen(ContextModal::Edit))
+    ]
+    .width(Fill)
+    .spacing(5)
+    .padding(5)
+}
+
+fn modal<'a, Message>(
+    base: impl Into<Element<'a, Message>>,
+    content: impl Into<Element<'a, Message>>,
+    on_blur: Message,
+) -> Element<'a, Message>
+where
+    Message: Clone + 'a,
+{
+    stack![
+        base.into(),
+        opaque(
+            mouse_area(center(opaque(content)).style(|_| {
+                container::Style {
+                    background: Some(
+                        Color {
+                            a: 0.0,
+                            ..Color::BLACK
+                        }
+                        .into(),
+                    ),
+                    ..container::Style::default()
+                }
+            }))
+            .on_press(on_blur)
+        )
+    ]
+    .into()
+}
+
+fn update(state: &mut State, message: Message) {
     match message {
         Message::RunAll => {
             state.sim.set_pc(0xC000);
@@ -129,33 +211,36 @@ fn update (state: &mut State, message: Message) {
             let text = state.editor_content.text();
             let _ = write![file, "{}", text];
             let _ = assemble("out", "out");
-            *state = State { 
+            *state = State {
                 sim: Simulator::bus_from_file("bin/out.bin"),
-                editor_content: text_editor::Content::with_text(&text.clone()) 
+                editor_content: text_editor::Content::with_text(&text.clone()),
             };
-        },
+        }
         _ => println!("Option for message: {:?} is not defined!", message),
     }
 }
 
-fn view (state: &State) -> Element<'_, Message> {
-
+fn view(state: &State) -> Element<'_, Message> {
     let inst_binary = column![text("binary placeholder")].height(Fill);
     let inst_box = column![editor_box(state), inst_binary];
     let regs_box = column![register_box(state)];
 
-    let interface = row![
-        inst_box.width(Fill).align_x(Alignment::Center),
-        regs_box.width(Fill).align_x(Alignment::Center),
-        memory_box(state)
-    ] .padding(10) .spacing(5);
-
-    interface.into()
+    let content = column![
+        row![context_menu()],
+        row![
+            inst_box.width(Fill).align_x(Alignment::Center),
+            regs_box.width(Fill).align_x(Alignment::Center),
+            memory_box(state)
+        ]
+        .padding(10)
+        .spacing(5)
+    ];
+    content.into()
 }
 
-fn main () -> iced::Result {
+fn main() -> iced::Result {
     iced::application("bobs8085-gui", update, view)
-        .theme(|_| iced::Theme::TokyoNight)
+        .theme(|_| theme())
         .centered()
         .run()
 }
